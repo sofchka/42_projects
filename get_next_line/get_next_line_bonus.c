8badc959bf32c1005668c2646563f7c1252c211c
Sofi Zakaryan <164770022+sofchka@users.noreply.github.com>
2025-03-19 03:05:16 +0400

    Add files via upload

---
+++ get_next_line/get_next_line_bonus.c
@@ -0,0 +1,74 @@
+#include "get_next_line_bonus.h"
+
+static void	*clean_storage(char *storage)
+{
+	if (storage)
+		free(storage);
+	return (NULL);
+}
+
+static char	*keep_storage(char *storage)
+{
+	char	*res;
+	char	*line;
+
+	line = ft_strchr(storage, '\n');
+	if (!line)
+		return (clean_storage(storage));
+	res = ft_strdup(line + 1);
+	clean_storage(storage);
+	return (res);
+}
+
+static char	*extract_line(char *storage)
+{
+	char	*line;
+
+	if (!storage || !*storage)
+		return (NULL);
+	line = ft_strchr(storage, '\n');
+	if (!line)
+		return (ft_substr(storage, 0, ft_strlen(storage)));
+	return (ft_substr(storage, 0, line - storage + 1));
+}
+
+static char	*read_file(int fd, char *storage)
+{
+	char	buffer[BUFFER_SIZE + 1];
+	ssize_t	bytes_read;
+	char	*new_storage;
+
+	bytes_read = 1;
+	while (!ft_strchr(storage, '\n') && bytes_read > 0)
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read <= 0)
+			break ;
+		buffer[bytes_read] = '\0';
+		new_storage = ft_strjoin(storage, buffer);
+		free(storage);
+		storage = new_storage;
+		if (!storage)
+			return (NULL);
+	}
+	if (bytes_read < 0)
+		return (clean_storage(storage));
+	return (storage);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*storage[FOPEN_MAX + 1];
+	char		*line;
+
+	if (fd < 0 || fd > FOPEN_MAX || BUFFER_SIZE <= 0)
+		return (NULL);
+	storage[fd] = read_file(fd, storage[fd]);
+	if (!storage[fd])
+		return (NULL);
+	line = extract_line(storage[fd]);
+	if (!line)
+		return (storage[fd] = clean_storage(storage[fd]));
+	storage[fd] = keep_storage(storage[fd]);
+	return (line);
+}
---
+++ get_next_line/get_next_line_utils.c
@@ -0,0 +1,101 @@
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s1)
+{
+	size_t	i;
+
+	i = 0;
+	while (s1 && s1[i])
+		++i;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	if (!s)
+		return (NULL);
+	while (*s)
+	{
+		if (*s == (char) c)
+			return ((char *)s);
+		s++;
+	}
+	if ((char) c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	size_t	size;
+	char	*sub;
+
+	if (!s)
+		return (NULL);
+	size = ft_strlen(s);
+	if (start >= size)
+	{
+		sub = (char *)malloc(sizeof (char));
+		if (!sub)
+			return (NULL);
+		sub[0] = 0;
+		return (sub);
+	}
+	if (len > size - start)
+		len = size - start;
+	sub = (char *)malloc((len + 1) * sizeof(char));
+	if (!sub)
+		return (NULL);
+	i = -1;
+	while (++i < len)
+		sub[i] = s[start + i];
+	sub[len] = '\0';
+	return (sub);
+}
+
+char	*ft_strdup(char *s)
+{
+	size_t	len;
+	char	*res;
+	int		i;
+
+	if (!s)
+		return (NULL);
+	len = ft_strlen(s);
+	res = (char *)malloc((len + 1) * sizeof(char));
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		res[i] = s[i];
+		++i;
+	}
+	res[i] = '\0';
+	return (res);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	i;
+	size_t	len1;
+	size_t	len2;
+
+	if (!s1 && !s2)
+		return (NULL);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	str = (char *)malloc((len1 + len2 + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	i = -1;
+	while (++i < len1)
+		str[i] = s1[i];
+	i = -1;
+	while (++i < len2)
+		str[len1 + i] = s2[i];
+	str[len1 + len2] = '\0';
+	return (str);
+}
