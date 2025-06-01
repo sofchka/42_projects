522cd56e7e981e10719fb39c3fff04af003a0c9e
Sofi <sofi@Sofis-MacBook-Air.local>
2025-06-02 00:31:55 +0400

    Clean up tracked ignored files

--- so_long/include/so_long.h
+++ so_long/include/so_long.h
@@ -5,6 +5,7 @@
 # include "../libft/libft.h"
 # include <fcntl.h>
 # include <stdlib.h>
+
 # include <stdio.h>
 
 # define ESC 65307
@@ -16,8 +17,33 @@ typedef struct s_game
 {
 	void	*mlx_ptr;
 	void	*mlx_win;
-	int		x;
-	int		y;
+	void	*mlx_img;
+	int		win_w;
+	int		win_h;
+	int		x_p;
+	int		y_p;
+	int		movement;
+	char	**map;
+	int		collect;
 }	t_game;
+
+typedef struct s_enemy {
+	int		x_e;
+	int		y_e;
+	int		sign;
+	int		x;
+	int		path_to_move;
+	int		sleep;
+	int		sleep_for_move;
+	char	*imgs[5];
+}	t_enemy;
+
+typedef struct s_map {
+	int	c;
+	int	p;
+	int	e;
+	int	x;
+	int	y;
+}	t_map;
 
 #endif
