153d9d6a82fd4e12baba31240d75da9d5cdf0eff
Sofi <sofi@Sofis-MacBook-Air.local>
2025-06-02 18:32:17 +0400

    strchr check added

--- so_long/libft/ft_strchr.c
+++ so_long/libft/ft_strchr.c
@@ -14,13 +14,15 @@
 
 char	*ft_strchr(const char *s, int c)
 {
+	if (!s)
+		return (NULL);
 	while (*s)
 	{
-		if (*s == (char)c)
+		if (*s == (char) c)
 			return ((char *)s);
 		s++;
 	}
-	if (*s == (char)c)
+	if ((char)c == '\0')
 		return ((char *)s);
 	return (NULL);
 }
